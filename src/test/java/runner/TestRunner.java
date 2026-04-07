c052021f5302a0b0d9482d196d097f3a4eb516c9
Bhushan <bhushan.mahire48@gmail.com>
2026-04-07 19:21:41 +0530

    Fix

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features",
+        features = "src/test/resources/login.feature",
         glue = "project2.stepDefinitions",
 
         plugin = {
